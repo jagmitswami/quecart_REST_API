bbeb6ffde3333e6ec1cda52a3cc84ededf26d354
Jagmit <bitscotch@gmail.com>
2023-07-15 18:20:03 +0530

    added API gateway

---
+++ quecart-microservices/api-gateway-quecart/src/main/java/com/quecart/ApiGatewayQuecartApplication.java
@@ -0,0 +1,13 @@
+package com.quecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayQuecartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayQuecartApplication.class, args);
+	}
+
+}
